cc0dcbaac66d90f838dbfdae4230bad2c0c7d936
adrezoff <selinag20042004@gmail.om>
2024-02-24 20:18:42 +0500

    fix logger exit and refactor code

--- src/main/java/org/robotgame/gui/buildingMenu/MainApplicationFrame.java
+++ src/main/java/org/robotgame/gui/buildingMenu/MainApplicationFrame.java
@@ -6,10 +6,14 @@ import org.robotgame.log.Logger;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 
 public class MainApplicationFrame extends JFrame {
     private final JDesktopPane desktopPane = new JDesktopPane();
+    private LogWindow logWindow = createLogWindow();
+    private GameWindow gameWindow = createGameWindow();
 
     public MainApplicationFrame() {
         int inset = 50;
@@ -17,15 +21,24 @@ public class MainApplicationFrame extends JFrame {
         setBounds(inset, inset, screenSize.width - inset * 2, screenSize.height - inset * 2);
         setContentPane(desktopPane);
 
-        LogWindow logWindow = createLogWindow();
         addWindow(logWindow);
-
-        GameWindow gameWindow = new GameWindow();
-        gameWindow.setSize(400, 400);
         addWindow(gameWindow);
 
         setJMenuBar(MenuBarBuilder.buildMenuBar());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        logWindow.addInternalFrameListener(new javax.swing.event.InternalFrameAdapter() {
+            @Override
+            public void internalFrameClosing(javax.swing.event.InternalFrameEvent e) {
+                closeLogWindow();
+            }
+        });
+        gameWindow.addInternalFrameListener(new javax.swing.event.InternalFrameAdapter() {
+            @Override
+            public void internalFrameClosing(javax.swing.event.InternalFrameEvent e) {
+                closeGameWindow();
+            }
+        });
     }
 
     protected LogWindow createLogWindow() {
@@ -33,12 +46,26 @@ public class MainApplicationFrame extends JFrame {
         logWindow.setLocation(10, 10);
         logWindow.setSize(300, 800);
         logWindow.pack();
-        Logger.debug("Протокол работает");
+        Logger.debug("Лог работает");
         return logWindow;
+    }
+
+    protected GameWindow createGameWindow() {
+        GameWindow gameWindow = new GameWindow();
+        gameWindow.setSize(400, 400);
+        return gameWindow;
     }
 
     protected void addWindow(JInternalFrame frame) {
         desktopPane.add(frame);
         frame.setVisible(true);
+    }
+
+    public void closeLogWindow() {
+        logWindow.dispose();
+    }
+
+    public void closeGameWindow() {
+        gameWindow.dispose();
     }
 }
