6e1851277d4ca533a0ddf5a5b847a8ff50f1d77d
MikhunS <150695058+MikhunS@users.noreply.github.com>
2024-04-15 22:07:08 +0500

    Create RobotModel.java

---
+++ robots/src/gui/RobotModel.java
@@ -0,0 +1,102 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.geom.AffineTransform;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.swing.*;
+
+import static gui.RobotModel.round;
+
+public class GameVisualizer extends JPanel
+{
+    public RobotModel robotModel = new RobotModel();
+    public RobotCoordinatesWindow robotCoordinatesWindow = new RobotCoordinatesWindow(robotModel);
+    private final Timer m_timer = initTimer();
+    private static Timer initTimer()
+    {
+        Timer timer = new Timer("events generator", true);
+        return timer;
+    }
+    public GameVisualizer()
+    {
+        m_timer.schedule(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                onRedrawEvent();
+            }
+        }, 0, 50);
+        m_timer.schedule(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                robotModel.onModelUpdateEvent();
+            }
+        }, 0, 10);
+        addMouseListener(new MouseAdapter()
+        {
+            @Override
+            public void mouseClicked(MouseEvent e)
+            {
+                robotModel.setTargetPosition(e.getPoint());
+                repaint();
+            }
+        });
+        setDoubleBuffered(true);
+    }
+    protected void onRedrawEvent()
+    {
+        EventQueue.invokeLater(this::repaint);
+    }
+    @Override
+    public void paint(Graphics g)
+    {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D)g;
+        drawRobot(g2d, round(robotModel.m_robotPositionX), round(robotModel.m_robotPositionY), robotModel.m_robotDirection);
+        drawTarget(g2d, robotModel.m_targetPositionX, robotModel.m_targetPositionY);
+    }
+    
+    private static void fillOval(Graphics g, int centerX, int centerY, int diam1, int diam2)
+    {
+        g.fillOval(centerX - diam1 / 2, centerY - diam2 / 2, diam1, diam2);
+    }
+    
+    private static void drawOval(Graphics g, int centerX, int centerY, int diam1, int diam2)
+    {
+        g.drawOval(centerX - diam1 / 2, centerY - diam2 / 2, diam1, diam2);
+    }
+    private void drawRobot(Graphics2D g, int x, int y, double direction)
+    {
+        int robotCenterX = round(robotModel.m_robotPositionX);
+        int robotCenterY = round(robotModel.m_robotPositionY);
+        AffineTransform t = AffineTransform.getRotateInstance(robotModel.m_robotDirection, robotCenterX, robotCenterY);
+        g.setTransform(t);
+        g.setColor(Color.MAGENTA);
+        fillOval(g, robotCenterX, robotCenterY, 30, 10);
+        g.setColor(Color.BLACK);
+        drawOval(g, robotCenterX, robotCenterY, 30, 10);
+        g.setColor(Color.WHITE);
+        fillOval(g, robotCenterX  + 10, robotCenterY, 5, 5);
+        g.setColor(Color.BLACK);
+        drawOval(g, robotCenterX  + 10, robotCenterY, 5, 5);
+    }
+    private void drawTarget(Graphics2D g, int x, int y)
+    {
+        AffineTransform t = AffineTransform.getRotateInstance(0, 0, 0); 
+        g.setTransform(t);
+        g.setColor(Color.GREEN);
+        fillOval(g, x, y, 5, 5);
+        g.setColor(Color.BLACK);
+        drawOval(g, x, y, 5, 5);
+    }
+}
