ddd83b5ae23188548c8bf730e06c82f09a725d4d
MikhunS <150695058+MikhunS@users.noreply.github.com>
2024-05-29 10:48:25 +0500

    Update GameWindow.java

--- robots/src/gui/GameWindow.java
+++ robots/src/gui/GameWindow.java
@@ -1,19 +1,24 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.util.ResourceBundle;
 
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
 
-public class GameWindow extends JInternalFrame
-{
+public class GameWindow extends JInternalFrame {
     public final GameVisualizer m_visualizer = new GameVisualizer();
-    public GameWindow()
-    {
-        super("Игровое поле", true, true, true, true);
+    //private ResourceBundle messages;
+
+    public GameWindow() {
+        super(MainApplicationFrame.messages.getString("game.window.title"), true, true, true, true);
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(m_visualizer, BorderLayout.CENTER);
         getContentPane().add(panel);
         pack();
+    }
+
+    public void updateTitle() {
+        setTitle(MainApplicationFrame.messages.getString("game.window.title"));
     }
 }
